96dc29db6c1307389d336dcec177a256d09fe74f
Vinicios Clarindo <vinicioslc@users.noreply.github.com>
2017-09-06 22:52:49 -0300

    Improved Templates

--- page-portfolio.php
+++ page-portfolio.php
@@ -9,22 +9,24 @@
         <?php 
             get_template_part( 'template', 'head-title' );
         ?>
+        <!-- CARDS  -->
         <article class="container light-blue">
-            <!-- CARDS  -->
             <div class="cards-container">
                 <?php 
+                $postsCategory = get_post_custom();
+                $postCatID = $postsCategory['posts_category_id'][0];
                 $wpquery = new WP_Query('type=post&posts_per_page=100&cat=2');
                 if ($wpquery->have_posts()) 
                 : while ( $wpquery->have_posts() ) 
                 : $wpquery->the_post();                  
-                get_template_part( 'template', 'gallery' );
+                get_template_part( 'template-card', 'gallery' );
             endwhile; else : 
                 ?>
                 <p><?php _e( 'Me desculpe, não pude encontrar nenhum post' ); ?></p>
                 <?php endif; ?>
             </div>
-            <!-- END CARDS -->
         </article>
+        <!-- END CARDS -->
         <!-- END CORPO -->
 <?php 
     get_footer();
