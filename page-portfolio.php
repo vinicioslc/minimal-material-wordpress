3fff2ae171ef11d2da62e41ad13b233c45ed84a7
Vinicios Clarindo <vinicioslc@users.noreply.github.com>
2017-09-11 10:00:02 -0300

    Refactoring

--- page-portfolio.php
+++ page-portfolio.php
@@ -13,12 +13,12 @@
         <article class="container light-blue">
             <div class="cards-container">
                 <?php 
-                    $postsCategory = get_post_custom();
-                    $postCatID = $postsCategory['posts_category_id'][0];
-                    $wpquery = new WP_Query('type=post&posts_per_page=100&cat='.$postCatID);
+                    $postFields = get_post_custom();
+                    $postCustomCategory = $postFields['posts_category_id'][0];
+                    $wpquery = new WP_Query('type=post&posts_per_page=100&cat='.$postCustomCategory .'');
                     if ($wpquery->have_posts()) 
                         : while ( $wpquery->have_posts() ) 
-                            : $wpquery->the_post();                  
+                            : $wpquery->the_post();        
                     get_template_part( 'template-card', 'project' );
                     endwhile; else : 
                 ?>
