bc9d215afab459fd732ca5626e132831f5e968ff
Vinicios Clarindo <vinicioslc@users.noreply.github.com>
2017-09-11 11:06:35 -0300

    Fixed Comments Section Printed Without Permission

--- template-post-comments.php
+++ template-post-comments.php
@@ -1,10 +1,14 @@
 
-<div id="comments" class="comments-area">
-    <h2 class="text-grey-darken-3"><?php  _e("Comments")?></h2>
-    <hr>
-    <?php 
-        if (comments_open()):
-            get_template_part( 'template','disqus');
-        endif;    
-    ?>
-<div>
+<?php 
+    if (comments_open()):
+?>
+    <div id="comments" class="comments-area">
+        <h2 class="text-grey-darken-3"><?php  _e("Comments")?></h2>
+        <hr>
+        <?php 
+                get_template_part( 'template','disqus');
+        ?>
+    <div>
+<?php
+    endif;
+?>
