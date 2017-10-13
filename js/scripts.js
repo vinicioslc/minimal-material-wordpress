15d35ef7650ea8e0c096d4a1b0ac047452481405
Vinicios Clarindo <vinicioslc@outlook.com>
2017-10-13 20:18:28 -0300

    Improved some css classes added posts images and text formating

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
     $(".card-category > a").removeAttr("href");
     $('body > #fademain').animate(
         {
             opacity: -1,
         },
-        1000,
+        1000, //após 1000ms remova o fade
         function () {
             $(this).remove();
         });
-        AddFadeToSidebar();
-        window.scrollTo(0, 0);
+    AddFadeToSidebar();
+    window.scrollTo(0, 0);
 }, { passive: true });
 
 //Animação ao abrir link            
 function openPage(value) {
-    
+
     var t = $('#menuButton').prop('checked', false);
     console.log(t);
     $('body').prepend('<div id="fademain" style="z-index: 4; opacity : 0; position : fixed; height : 100vh; width:100vw; background:white;"></div>')
@@ -28,12 +27,12 @@ function openPage(value) {
             window.open(value, "_self");
         });
 }
-
-    function AddFadeToSidebar() {
-        var elements = $('a');
-        elements.each (function (obj, $val) {
-            var link = $($val).attr("href");
-            $($val).click(function () { openPage(link); });
-            $($val).removeAttr('href').css('cursor', 'pointer');
-        });
+// adiciona animação de fade ao clicar nos links especificados abaixo
+function AddFadeToSidebar() {
+    var elements = $('.menu-item > a, .page_item > a, .bread-crumbs > a');
+    elements.each(function (obj, $val) {
+        var link = $($val).attr("href");
+        $($val).click(function () { openPage(link); });
+        $($val).removeAttr('href').css('cursor', 'pointer');
+    });
 }
