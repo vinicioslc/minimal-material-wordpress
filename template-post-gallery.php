7e9a7e242a28d60def3f7ae191d0099fed49168d
Vinicios Clarindo <vinicioslc@outlook.com>
2017-10-21 22:53:30 -0200

    Updated back Link

--- template-post-gallery.php
+++ template-post-gallery.php
@@ -1,34 +1,34 @@
 <?php 
     // This thing get the paper-header color
-    $customPostField = get_post_custom();
-    $headerColor = $customPostField['paper-header-color'][0];
+$customPostField = get_post_custom();
+$headerColor = $customPostField['paper-header-color'][0];
 ?>
 
 <div class="paper-gallery-bg" style="background : <?= $headerColor ?>;">
     <article class="paper-container grey-lighten-1 scroll-animation text-white">
     <!-- CARDS  -->
-            <a href="<?= get_home_url(); ?>">
+            <a href="<?php echo (get_home_url() . '/' . the_category()); ?>">
                 <div class="icon">
                     <div class="back-post-icon"></div>                  
                 </div>
             </a>
                 <?php 
-                    if ( have_posts() ) : while ( have_posts() ) : the_post();  
+                if (have_posts()) : while (have_posts()) : the_post();
                 ?>  
             <div class="content text-white">
                 <?php 
-                    the_content();
+                the_content();
                 ?>
             </div>                         
             <?php 
-                get_template('template-post-comments');
-                    endwhile; else : 
+            get_template('template-post-comments');
+            endwhile; else :
             ?>
             <p>
-                <?php _e( 'Me desculpe, nenhum post foi achado.' ); ?>
+                <?php _e('Me desculpe, nenhum post foi achado.'); ?>
             </p>
             <?php
-                endif;
+            endif;
             ?>   
     </article>
 </div>    
