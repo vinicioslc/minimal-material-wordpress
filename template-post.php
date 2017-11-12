1351d73fa31c5e786741e553b25768aafce8e260
Vinicios Clarindo <vinicioslc@outlook.com>
2017-11-12 15:45:23 -0200

    Improved bread crumbs with date section Improved bubble Animation

--- template-post.php
+++ template-post.php
@@ -1,21 +1,31 @@
 
 <?php 
-    get_template_part( 'template-paper', 'header-color' );
+    get_template_part('template-paper', 'header-color');
 ?>
 
 <article class="paper-container grey-lighten-1 scroll-animation">
 <!-- CARDS PAGE TEXT -->
     <div class="paper white">
-            <div class="bread-crumbs text-grey lighten-2">
-                    <a href="<?= get_home_url( null, 'blog', null ); ?>">
-                    BLOG &gt; &gt; 
+        <div class="crumbs-container">
+
+            <div class="bread-crumbs text-grey lighten-2 text-left">
+                <a href="<?= get_home_url(null, 'blog', null); ?>">
+                        <?php 
+                        echo bloginfo('name');
+                        ?> &gt; 
                     </a>
                     <?php the_title(); ?>                    
+                </div>
+                <div class="date-crumbs text-grey lighten-2 text-right">
+                    <a><?php 
+                        the_time('l, j, F, Y');
+                        ?></a>
+                </div>
             </div>
             <?php 
-            if ( have_posts() ) :
-                while ( have_posts() ) :
-                    the_post();  
+            if (have_posts()) :
+                while (have_posts()) :
+                the_post();
             ?>
             <div class="content text-black">
                 <h2>
@@ -24,18 +34,19 @@
                     ?>
                 </h2>
                 <?php 
-                    the_content();
+                the_content();
                 ?>
             </div>             
         <?php
-            endwhile; else : 
-                ?>
+        endwhile;
+        else :
+        ?>
         <p>
-            <?php _e( 'Me desculpe, nenhum post atende os critérios.' ); ?>
+            <?php _e('Me desculpe, nenhum post atende os critérios.'); ?>
         </p>
         <?php
-            endif;
-            get_template_part( 'template-post-comments' );
+        endif;
+        get_template_part('template-post-comments');
         ?>  
     </div>
 </article>
